809719e6ab1d82e01822047732d30142131b5310
tnantoka <tnantoka@bornneet.com>
2017-03-01 20:28:03 +0900

    Delete comment out

--- Sources/AppLogic/Droplet+Setup.swift
+++ Sources/AppLogic/Droplet+Setup.swift
@@ -3,7 +3,6 @@ import Vapor
 public let title = "Server-Side Swift as a Live Example"
 
 public func load(_ drop: Droplet) {
-    // drop.get { _ in return "hello" }
     drop.get { req in
         return try drop.view.make("welcome", [
             "title": title
